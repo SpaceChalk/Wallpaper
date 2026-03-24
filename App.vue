2fd0d431904d4a4b0474ece33641447436a658bf
SpaceChalk <147881001+SpaceChalk@users.noreply.github.com>
2026-03-24 23:14:48 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Clock from "@/components/Clock.vue";
+import Todo from "@/components/Todo.vue";
+</script>
+
+<template>
+  <div>
+    <Todo/>
+  </div>
+  <Clock/>
+</template>
+
+<style scoped>
+
+</style>
